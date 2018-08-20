2d2c1974fff2f3cd64d2b0f24e72f4412f9e7dfb
marcin <marcin.kasinski@gmail.com>
2018-08-20 12:24:21 +0200

    .

--- docker/start.sh
+++ docker/start.sh
@@ -3,4 +3,4 @@
 ls -l /
 cat /etc/init.d/filebeat
 /etc/init.d/filebeat start
-java -jar SpringBootZipkin-0.0.1-SNAPSHOT.jar
+java -jar SpringBootZipkin-0.0.1-SNAPSHOT.jar --logging.level.zipkin2=DEBUG
